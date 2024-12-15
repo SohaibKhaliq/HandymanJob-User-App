a7a6e87bfef5e5db48bfce1df8150d69cd12c83a
Sohaib Khaliq <sohaibkhaliq510@gmail.com>
2024-12-15 15:56:12 +0500

    Rename package from com.iqonic.servicebooking to sohaib.user.handymanjob

--- android/app/src/main/kotlin/com/iqonic/servicebooking/MainActivity.kt
+++ android/app/src/main/kotlin/com/iqonic/servicebooking/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.iqonic.servicebooking
+package sohaib.user.handymanjob
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
